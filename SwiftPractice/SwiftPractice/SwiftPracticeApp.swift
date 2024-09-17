aa0b683bebeface96d139ceff8c6d47023c639bf
blurfree <blurfree2521@gmail.com>
2024-09-17 13:11:07 +0900

    Create New Project / 새 프로젝트 생성

---
+++ SwiftPractice/SwiftPractice/SwiftPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPracticeApp.swift
+//  SwiftPractice
+//
+//  Created by blurfree on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
